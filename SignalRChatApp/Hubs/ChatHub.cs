1760cce21da50ea17d6c0c7687b3dfbc8b551834
Ahmed Abdulrahman Alfadhel <a.alfadhel@ncgr.gov.sa>
2024-09-17 10:53:46 +0300

    modify code to be available every day

--- SignalRChatApp/Hubs/ChatHub.cs
+++ SignalRChatApp/Hubs/ChatHub.cs
@@ -1,15 +1,52 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace SignalRChatApp.Hubs
 {
 
     public class ChatHub : Hub
     {
+        // In-memory storage for chat messages
+        private static List<string> _messages = new List<string>();
+
+        // Timer to reset chat history every 24 hours
+        private static System.Timers.Timer _resetTimer;
+
+        // Static constructor to set up the daily reset timer
+        static ChatHub()
+        {
+            _resetTimer = new System.Timers.Timer
+            {
+                Interval = 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+                AutoReset = true,
+                Enabled = true
+            };
+            _resetTimer.Elapsed += (sender, e) => ClearChatHistory();
+        }
+
+        // Method to clear chat history
+        private static void ClearChatHistory()
+        {
+            _messages.Clear();
+        }
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // Store message in the list
+            var fullMessage = $"{user}: {message}";
+            _messages.Add(fullMessage);
+
+            // Send message to all connected clients
+            await Clients.All.SendAsync("ReceiveMessage", fullMessage);
+        }
+
+        public async Task LoadChatHistory()
+        {
+            // Send existing chat history to the connecting client
+            foreach (var message in _messages)
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", message);
+            }
         }
     }
 }
- 
